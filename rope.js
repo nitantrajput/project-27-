c1832bc860768ef082bf0f7d3ac21843c2a53a82
nitantrajput <73350068+nitantrajput@users.noreply.github.com>
2020-12-11 15:24:43 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -3,18 +3,19 @@ class Rope{
         this.offsetX = offsetX
         this.offsetY = offsetY
         var options = {
-            body1: body1,
-            body2: body2,
-            pointB:{x:this.offsetX, y:this.offsetY}
+            bodyA: body1,
+            bodyB: body2,
+            pointB:{x:this.offsetX, y:this.offsetY},
+            length:150
         }
-        this.rope = Constraint.create(options);
+        this.rope = Matter.Constraint.create(options);
         World.add(world, this.rope);
     }
     display(){
         
-            var bodyA = this.rope.bodyA.position;
-            var bodyB = this.bodyB;
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
             strokeWeight(4);
-            line(bodyA.x, bodyA.y, bodyB.x, bodyB.y);
+            line(pointA.x, pointA.y, this.offsetX, this.offsetY);
         }
     }
